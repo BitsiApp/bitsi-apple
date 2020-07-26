200545f4a6398e4099748574634ba02b40130b08
BJ Miller <bj@sixfivesoftware.com>
2020-07-26 18:33:36 -0400

    Initial Commit

---
+++ Shared/BitsiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitsiApp.swift
+//  Shared
+//
+//  Created by BJ Miller on 7/26/20.
+//
+
+import SwiftUI
+
+@main
+struct BitsiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
